e836e44755862180a6c36d07552a2d4ab3781704
ddeboe4 <ddeboe4@wgu.edu>
2026-02-01 18:08:09 -0700

    correct copy/paste error

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/train_val_test_split",
+                f"{config['main']['components_repository']}/test_regression_model",
                 'main',
                 parameters={
                     'mlflow_model': 'nyc_airbnb/random_forest_export:prod',
